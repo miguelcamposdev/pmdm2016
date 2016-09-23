7f60beb65ad165dcca88e46df3408c95bbaf117b
jjsosa <sosaosuna@gmail.com>
2016-09-23 12:56:32 +0200

    borro el main

--- 01_Activity_Intent/02_DosActivities/app/src/main/java/com/miguelcr/basics/MainActivity.java
+++ 01_Activity_Intent/02_DosActivities/app/src/main/java/com/miguelcr/basics/MainActivity.java
@@ -1,13 +1,1 @@
-package com.miguelcr.basics;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
