0fe0940333c6de3e3e56f4517d508328e081f21c
Miguel Campos <camposmiguel@gmail.com>
2016-09-23 12:50:59 +0200

    - PRoyecto de navegación entre 2 acitivities

---
+++ 01_Activity_Intent/02_DosActivities/app/src/main/java/com/miguelcr/basics/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.miguelcr.basics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+
+    public void onClickHome(View view) {
+        Log.i("INFO METODO","Ha hecho click en Ir a home");
+    }
+}
